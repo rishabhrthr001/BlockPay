13e263b706a2e2c3feeb6441885973e86c3b5c40
rishabhrthr001 <rishabh.rthr001@gmail.com>
2025-08-22 23:27:33 +0530

    final commit

--- frontend/src/adminComponents/AdminDashboard.jsx
+++ frontend/src/adminComponents/AdminDashboard.jsx
@@ -4,12 +4,14 @@ import { useNavigate } from "react-router-dom";
 const AdminDashboard = () => {
   const navigate = useNavigate();
   return (
-    <button
-      onClick={() => navigate("/set-salary")}
-      className="px-6 py-2 rounded-lg bg-black text-white font-semibold hover:bg-gray-900 transition"
-    >
-      Set Salaries
-    </button>
+    <div className="bg-[#242424] min-h-screen flex items-center ">
+      <button
+        onClick={() => navigate("/set-salary")}
+        className="px-6 py-2 rounded-lg bg-black text-white font-semibold hover:bg-gray-900 transition"
+      >
+        Set Salaries
+      </button>
+    </div>
   );
 };
 
